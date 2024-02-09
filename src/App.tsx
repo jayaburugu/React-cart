2dd17d151a039128405261e2f8d374e8f20e7d81
Jay Burugupalli <113130081+jayaburugu@users.noreply.github.com>
2024-02-09 11:32:42 +0530

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+/*!
+ * @author Mohamed Muntasir
+ * @link https://github.com/devmotheg
+ */
+
+import React from "react";
+
+import Project from "./components/Project";
+
+const App = () => (
+  <React.Fragment>
+    <Project />
+    <a
+      className="block mx-auto mt-6 mb-4 text-sm font-bold w-fit"
+      href="https://github.com/akanksha356"
+    >
+      Coded by Akanksha Muraboyina
+    </a>
+  </React.Fragment>
+);
+
+export default App;
